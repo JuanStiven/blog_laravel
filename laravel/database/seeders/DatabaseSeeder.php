c429a446cddbba0342bdca0ef45adef3025f30a2
Yeider Adrian Mina Caicedo <jose.mera@adwise.cl>
2022-08-30 11:13:48 -0500

    fakers and seeders

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Categorie;
+use App\Models\Image;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,8 +24,17 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
+        //     'name' => 'Test User',s
         //     'email' => 'test@example.com',
         // ]);
+
+
+        Storage::deleteDirectory('public/posts');
+        Storage::makeDirectory('public/posts');
+
+        $this->call(UserSeeder::class);
+        Categorie::factory(4)->create();
+        Tag::factory(8)->create();
+        $this->call(PostSeeder::class);
     }
 }
